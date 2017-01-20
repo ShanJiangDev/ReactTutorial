f6be8ce7457799e16b93bcd1a2b70ff47d9628b4
Shan Jiang <Shan.Jiang@cybercom.com>
2017-01-20 12:26:45 +0100

    How to use variable as javascript

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import Layout from "./components/Layout";
+{/* Only return one element at the time */}
+class Layout extends React.Component {
+	render(){
+		const name = "will";
+		return(
+			<div>
+				<h1>It working123!</h1>
+				<h1>It working123!</h1>
+				<p> its {name} </p>
+				<p> its {1+2}</p>
+			</div>
+		);
+	}
+}
 
 const app = document.getElementById('app');
+{/*Render Layout into App*/}
 ReactDOM.render(<Layout/>, app);
+
+
