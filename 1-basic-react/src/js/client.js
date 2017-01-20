4771e21876903d2ed2eecc79cd80b65d4a88fc7a
Shan Jiang <Shan.Jiang@cybercom.com>
2017-01-20 12:59:03 +0100

    How to add use function for Component

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 {/* Only return one element at the time */}
 class Layout extends React.Component {
+	getVale(){
+		return 9;
+	}
+	getSecondVal(val){
+		return val*val;
+	}
 	render(){
 		const name = "will";
 		return(
@@ -10,6 +16,8 @@ class Layout extends React.Component {
 				<h1>It working123!</h1>
 				<p> its {name} </p>
 				<p> its {1+2}</p>
+				<p> number from function: {this.getVale()} </p>
+				<p> double amount of 2: {this.getSecondVal(2)} </p>
 			</div>
 		);
 	}
