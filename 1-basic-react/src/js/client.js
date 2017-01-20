e5a6786ad25effb3ba0487be481784b2627bce11
Shan Jiang <Shan.Jiang@cybercom.com>
2017-01-20 13:01:08 +0100

    How to use constructor value inside render

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 {/* Only return one element at the time */}
 class Layout extends React.Component {
+	constructor(){
+	{/*Super() is need everytime for the first line inside constructor*/}
+		super();
+		this.name = "Shan"
+	}
 	getVale(){
 		return 9;
 	}
@@ -18,6 +23,7 @@ class Layout extends React.Component {
 				<p> its {1+2}</p>
 				<p> number from function: {this.getVale()} </p>
 				<p> double amount of 2: {this.getSecondVal(2)} </p>
+				<p> name from constructor: {this.name}</p>
 			</div>
 		);
 	}
