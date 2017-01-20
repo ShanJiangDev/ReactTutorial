fb6049b7d29ed35ab78cf6e14943b9c44dc8ddcf
Shan Jiang <Shan.Jiang@cybercom.com>
2017-01-20 13:53:35 +0100

    Add a component into another component
    Seperation Tittle(text) from Header Component

--- 1-basic-react/src/js/Component/Header.js
+++ 1-basic-react/src/js/Component/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Tittle from "./Header/Tittle";
 {/*Component's name always start with capital letter*/}
 {/* Only return one element at the time */}
 export default class Header extends React.Component {
 	render(){
 		const name = "will";
 		return(
-			<header>header</header>
+			<div>
+				<header>header</header>
+				<Tittle />
+			</div>
 		);
 	}
 }
---
+++ 1-basic-react/src/js/Component/Header/Tittle.js
@@ -0,0 +1,10 @@
+import React from "react";
+{/*Component's name always start with capital letter*/}
+{/* Only return one element at the time */}
+export default class Tittle extends React.Component {
+	render(){
+		return(
+			<h1> Welcome Header Tittle </h1>
+		);
+	}
+}
